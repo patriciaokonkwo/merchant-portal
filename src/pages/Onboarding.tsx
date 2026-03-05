b9437785d85752cc50388eb5108e4d606a49e759
patriciaokonkwo <patriciaokonkwo@walla.co.il>
2026-03-05 15:18:22 +0200

    seed: src/pages/Onboarding.tsx

---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function OnboardingPage() {
+  return (
+    <div>
+      <h1>Welcome to NovaPay</h1>
+      <p>Complete the steps below to start accepting payments.</p>
+    </div>
+  );
+}
